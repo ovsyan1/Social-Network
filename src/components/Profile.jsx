e07c9077b50af9f46efcc7427f744d06fc114c58
Кирило Овсяннiк <kovsiannik@e1r10p7.unit.ua>
2021-07-16 10:48:09 +0300

    start big project

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+                <div>
+                    <img height="300px" src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"></img>
+                </div>
+                <div>
+                ava + description
+                </div>
+                <div>
+                My posts
+                <div>
+                    New post
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+                </div>
+            </div>
+}
+
+export default Profile;

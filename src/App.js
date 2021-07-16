7f1a0c151cf99f26b9bb8e0ad2af035b26c9d426
Кирило Овсяннiк <kovsiannik@e1r10p7.unit.ua>
2021-07-16 11:28:27 +0300

    css-modules

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import Navbar  from './components/Navbar';
 import Profile from './components/Profile';
 
-
 function App() {
   return (
     <div className="app-wrapper">
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import classes from './Profile.module.css';
+
 
 const Profile = () => {
-    return <div className="content">
+    return <div className={classes.content}>
                 <div>
                     <img height="300px" src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"></img>
                 </div>
@@ -10,10 +12,14 @@ const Profile = () => {
                 </div>
                 <div>
                 My posts
-                <div>
+                <div className={classes.posts}>
                     New post
-                    <div>post1</div>
-                    <div>post2</div>
+                    <div className={classes.item}>
+                        post1
+                    </div>
+                    <div className={classes.item}>
+                        post2
+                    </div>
                 </div>
                 </div>
             </div>
